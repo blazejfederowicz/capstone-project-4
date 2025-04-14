29e944ca511ec3660bcbfcbb7ff5a760d941c061
Błażej <wargun364@gmail.com>
2025-04-14 23:42:20 +0200

    animation

---
+++ public/animation.js
@@ -0,0 +1,35 @@
+import { animate, hover } from "https://cdn.jsdelivr.net/npm/motion@latest/+esm";
+
+let windowX = window.scrollX
+let windowY = window.scrollY
+
+const updateScrollState = () => {
+    windowX = window.scrollX;
+    windowY = window.scrollY;
+};
+
+document.addEventListener('scroll', updateScrollState);
+
+document.addEventListener('mousemove', (e) => {
+    const glow = document.querySelector('.glow-effect');
+    const glowUp = document.querySelector('.glow-effect2')
+
+    const adjustedX = windowX + e.clientX;
+    const adjustedY = windowY + e.clientY;
+
+    animate(glow,{left:adjustedX, top:adjustedY}, {duration: 0})
+    animate(glowUp,{left:adjustedX, top:adjustedY}, {duration: 0})
+    });
+
+window.addEventListener("DOMContentLoaded", () => {
+    hover(".opacity-container", (element) => {
+        animate('.glow-effect', { opacity: 1 },{ease:[ "easeIn"]});
+        animate('.glow-effect2', { opacity: 1 }, {ease:[ "easeOut"], delay:0.2});
+
+        return () => {
+            animate('.glow-effect', { opacity: 0 },{duration:0.05,ease:[ "easeOut"]})
+            animate('.glow-effect2', { opacity: 0 },{ease:[ "easeOut"]});
+        };
+    });
+});
+
